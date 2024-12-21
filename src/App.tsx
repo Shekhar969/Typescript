960a83aa37efcecb56e0af8a36d00dcc8672da9c
Shekhar969 <shekharrawal96@gmail.com>
2024-12-21 19:16:27 +0545

    Each variable must have a caterogery

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
 
+let name:string ="Shekhar";
+let age:number=21;
+let isGoodBoy:boolean=true;
+let hobbies:string[];
+let role:[string,number];
+
 function App() {
 
   return (
     <>
-     <h1>Hello World</h1>
+    <h1>{name}</h1>
+    <h1>{age}</h1>
+    <h1>{isGoodBoy }</h1>
+    <h1>{hobbies =["Playinggames " ," Eating ", " Sleeping"]}</h1>
+    <h1>{role=["playinggames" , 69]}</h1>
     </>
   )
 }
